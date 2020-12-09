0b08c6901245a1b1b39d9198831d4d2576d875a7
mrjarvis698 <dudamajaykumar59@gmail.com>
2020-12-09 01:33:14 -0800

    msm8956-common: Build and Add android.hardware.weaver@1.0 in manifest
    log-
    hwservicemanager: getTransport: Cannot find entry android.hardware.weaver@1.0::IWeaver/default in either framework or device manifest.
    
    Signed-off-by: mrjarvis698 <dudamajaykumar59@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -462,6 +462,11 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/android.frameworks.sensorservice@1.0-v27.so \
     prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/android.hardware.gnss@1.0-v27.so
 
+# Weaver
+PRODUCT_PACKAGES += \
+    android.hardware.weaver@1.0-impl \
+    android.hardware.weaver@1.0-service
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
