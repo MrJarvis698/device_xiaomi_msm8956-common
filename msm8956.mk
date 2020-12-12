371fb82126da2f9ca0150550a4e0194fa3a3d3d6
markakash <akashniki@gmail.com>
2020-12-11 22:56:59 -0800

    msm8956-common: build and add android.hardware.thermal@2.0 log- hwservicemanager: getTransport: Cannot find entry android.hardware.thermal@2.0::IThermal/default in either framework or device manifest.
    Author:    markakash <akashniki@gmail.com>
    Signed-off-by: mrjarvis698 <dudamajaykumar59@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -418,6 +418,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
+    android.hardware.thermal@2.0-impl \
+    android.hardware.thermal@2.0-service \
     thermal.msm8952
 
 # USB HAL
