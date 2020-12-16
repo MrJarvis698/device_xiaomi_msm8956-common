9eb01413ed46141ce25637614c5e7f1024eb7a1c
Author:    trkzmn <trkzmn89@gmail.com>
2020-12-16 01:41:40 -0800

    msm8956-common: Switch power aidl hal
    Author:    trkzmn <trkzmn89@gmail.com>
    Signed-off-by: mrjarvis698 <dudamajaykumar59@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -332,7 +332,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power-service
 
 # Qualcomm dependencies
 PRODUCT_PACKAGES += \
