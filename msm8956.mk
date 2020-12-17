dca3a6037317392a9e69c46ed6772b0271f469d1
mrjarvis698 <dudamajaykumar59@gmail.com>
2020-12-16 19:42:08 -0800

    msm8956-common: DRM: Enable 1.3 Clearkey and Widevine

--- msm8956.mk
+++ msm8956.mk
@@ -190,7 +190,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # DumpState
 PRODUCT_PACKAGES += \
@@ -420,7 +420,7 @@ PRODUCT_PACKAGES += \
 
 # Widevine
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-service.widevine
+    android.hardware.drm@1.3-service.widevine
 
 # VR
 PRODUCT_PACKAGES += \
