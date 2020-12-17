461bb99e01915971fbbed2f7abf2589ec8d42f76
dianlujitao <dianlujitao@gmail.com>
2020-12-17 10:09:50 -0800

    msm8956-common: Add prebuilt protobuf from sdk28
    Author:    dianlujitao <dianlujitao@gmail.com>
    Signed-off-by: mrjarvis698 <dudamajaykumar59@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -457,7 +457,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/android.frameworks.sensorservice@1.0-v27.so \
-    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/android.hardware.gnss@1.0-v27.so
+    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/android.hardware.gnss@1.0-v27.so \
+    prebuilts/vndk/v28/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v28.so
 
 # Wifi
 PRODUCT_PACKAGES += \
